1f58acb68d23196771e9085d5afca5cb5f976cb4
Justin Scott & Alyssa Horrocks <justinandalyssa@fake.come>
2015-08-10 09:37:31 -0700

    Pass spec identifying isoceles triangles

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -8,9 +8,11 @@ class Triangle
   define_method(:triangle?) do
     if @side_a == @side_b && @side_b == @side_c
       true
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+      true
     else
       false
-    end  
+    end
   end
 
 end
